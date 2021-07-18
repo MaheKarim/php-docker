27d41f323713226571147e8011fab719c49339ad
Mahe Karim <mahekarim@deligram.com>
2021-07-18 06:46:12 +0600

    Constructor Method

--- www/html/oop/class_construct_method.php
+++ www/html/oop/class_construct_method.php
@@ -3,18 +3,25 @@
         public $name;
         public $semanticVersion;
 
-        public function __construct($name, $semanticVersion)
+        public function __construct($name, $semanticVersion = 0)
         {
             // Class to Obj, This Method Will Run Once Automatically
-            echo "This is Constructor. My Version is ";
+            echo "\n This is Constructor. My Version is ";
             $this->name = $name;
             $this->semanticVersion = $semanticVersion;
         }
-        
+
+        public function info()
+        {
+            $this->version();
+        }
         public function version()
         {
-            echo $this->name ;
-            echo $this->semanticVersion ;
+            if ($this->semanticVersion == 0) {
+                echo $this->name , $this->semanticVersion ;
+            } else {
+                echo "$this->name This is Supporting now $this->name , $this->semanticVersion ";
+            }
         }
 
     }
@@ -24,4 +31,6 @@
 //    $frameWork->version();
 
     $frameWork = new Laravel("8.0.", "12.69");
-    $frameWork->version();
+    $frameWork->info();
+//    $newFramework = new Laravel("12.0");
+//    $newFramework->info();
