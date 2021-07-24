aa2737364c3ca585238bd05a680c5415d0e13097
Mahe Karim <mahekarim@deligram.com>
2021-07-25 00:54:57 +0600

    GET_SET Magic methods

--- www/html/oop/magic_methods.php
+++ www/html/oop/magic_methods.php
@@ -1,2 +1,33 @@
 <?php
+    class Student {
 
+        private $name;
+        private $age;
+        private $class;
+
+        function __construct($name='', $age='', $class=''){
+            $this->name = $name;
+            $this->age = $age;
+            $this->class = $class;
+        }
+
+        public function __get($property)
+        {
+            return strtoupper($this->$property);
+        }
+
+        public function __set($property, $value)
+        {
+            return $this->$property = $value;
+        }
+    }
+
+    $assignSomething = new Student('Mahe', '174', '7th');
+
+    $assignSomething->name = "Jamal\n ";
+
+    echo $assignSomething->name;
+
+    print "Show name: {$assignSomething->name}\n";
+    print "Show age: {$assignSomething->age}\n";
+    print "Show class: {$assignSomething->class}";
